3a222eb32267b1327772c72054257f640161063d
gil <gilgil.abramovich@gmail.com>
2026-03-05 02:21:35 +0200

    return to what it was. nx adds a whole lot of caching that we don't want to deal with, so I use the function the "nx start"  does directly.

--- bigProject/bisect_runner_script.sh
+++ bigProject/bisect_runner_script.sh
@@ -4,7 +4,7 @@ TEST_DIR="C:\\פרויקטים\\Test2"
 echo "Starting runner script"
 
 C:/פרויקטים/Test2/node_modules/.bin/start-server-and-test \
-  "cd bigProject/apps/client && npm install && cd ../../../ && npx nx run client:start" \
+  "cd bigProject/apps/client && npm install && npm run dev \
   http://localhost:5180 \
   "cd C:/פרויקטים/Test2 && npx jest bigProject/test/divide.test.js"
 echo "Finishing runner script"
