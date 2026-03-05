08d7a1d33f34662553282ab9cbd8464a19551a28
gil <gilgil.abramovich@gmail.com>
2026-03-05 13:24:31 +0200

    add fix

--- bigProject/bisect_runner_script.sh
+++ bigProject/bisect_runner_script.sh
@@ -4,7 +4,7 @@ TEST_DIR="C:\\פרויקטים\\Test2"
 echo "Starting runner script"
 
 C:/פרויקטים/Test2/node_modules/.bin/start-server-and-test \
-  "cd bigProject/apps/client && npm install && npm run dev \
+  "cd bigProject/apps/client && npm install && npm run dev" \
   http://localhost:5180 \
   "cd C:/פרויקטים/Test2 && npx jest bigProject/test/divide.test.js"
 echo "Finishing runner script"
