7ca9b0cfffd8a8539a58daa45f48eb7666dce3e0
gil <gilgil.abramovich@gmail.com>
2026-03-05 13:30:26 +0200

    minor

--- bigProject/bisect_runner_script.sh
+++ bigProject/bisect_runner_script.sh
@@ -6,6 +6,5 @@ echo "Starting runner script"
 C:/פרויקטים/Test2/node_modules/.bin/start-server-and-test \
   "cd bigProject/apps/client && npm install && npm run dev" \
   http://localhost:5180 \
-  "cd C:/פרויקטים/Test2 && npx jest bigProject/test/divide.test.js"
-echo "Finishing runner script"
+  "cd C:/פרויקטים/Test2 && npx jest bigProject/test/divide.test.js --maxWorkers=1 --forceExit"
 exit $?
